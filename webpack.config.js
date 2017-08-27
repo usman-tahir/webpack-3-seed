c6ef5158c23f2eabda41de0444918a6b05b53018
Usman Tahir <tahir.usman.ali94@outlook.com>
2017-08-27 14:58:08 -0400

    added Babel/ES6 dev dependencies and loading rules

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,20 @@ const path = require('path')
 const webpack = require('webpack')
 
 module.exports = {
+    rules: [
+        {
+            test: /\.js$/,
+            exclude: [/node_modules/, /dist/],
+            use: [
+                {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            ]
+        }
+    ],
     plugins: [
         new webpack.optimize.CommonsChunkPlugin({
             name: 'commons',
